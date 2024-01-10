15578d9d4e221f65a777e66c0134246a112d0018
arturomeyberg <c.a.gonzalez@cgiar.org>
2024-01-10 19:49:56 +0000

    Create src folder

---
+++ src/r-capabilities/main.py
@@ -0,0 +1,43 @@
+
+
+import xarray as xr
+import pandas as pd
+import boto3
+import numpy as np
+import s3fs
+import utils
+from fastapi import FastAPI
+from model.models import getDataRequest
+from fastapi.responses import FileResponse
+import random
+
+
+
+
+
+s3Key = utils.readConfiguration('read-access-keys','key')
+s3Secret = utils.readConfiguration('read-access-keys','secret')
+s3 = s3fs.S3FileSystem(key= s3Key
+                       ,secret=s3Secret)
+
+app = FastAPI(docs_url="/")
+@app.post('/v1/getdataOnePoint')
+async def getData(request: getDataRequest):
+    s3Path = utils.readConfiguration('zarr-path',request.variableName)
+    rangeOfDates = pd.date_range(start=request.startDt,end=request.endDt)
+    zarrData = utils.initializeZarrConnection(s3Path,s3)
+    zarrData = utils.filterData(zarrData,
+                                rangeOfDates,
+                                request.lat,
+                                request.lon
+                               )
+    fileName = str(random.randint(0,250655))
+    
+    zarrData.to_netcdf(f'{fileName}.nc')
+    return FileResponse(f'{fileName}.nc')
+    
+    
+    
+
+
+
---
+++ src/r-capabilities/utils.py
@@ -0,0 +1,37 @@
+import yaml
+import xarray as xr
+import pandas as pd
+import s3fs
+
+def readConfiguration(propertie:str,value:str)->str:
+    with open('conf/Cloud.yml','r') as file:
+        ymlFile = yaml.safe_load(file)
+    
+    return ymlFile[propertie][value]
+
+def initializeZarrConnection( s3Path: str
+                             ,s3 
+                             )->xr.Dataset:
+    """
+    Initializes a Zarr connection using an S3 path and S3 client.
+
+    Parameters:
+    - s3Path (str): The S3 path to the Zarr store.
+    - s3: An S3 client object.
+
+    Returns:
+    - xarray.Dataset: The Zarr data as an xarray dataset.
+    """
+    zarrMap = s3fs.S3Map(root=s3Path, s3=s3, check=False)
+    zarrData = xr.open_zarr(store=zarrMap, consolidated=True)
+    return zarrData
+    
+def filterData(zarrData,rangeOfDates
+               ,lat:float
+               ,lon:float)->xr.Dataset:
+    zarrData = zarrData.sel(time = rangeOfDates)
+    zarrData = zarrData.sel(lat = lat,
+                            lon = lon,
+                            method = 'nearest'
+                           )
+    return zarrData
