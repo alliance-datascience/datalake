585b5c9ef4f5fae77c218b2f8bd7f76b1cdb8170
arturomeyberg <c.a.gonzalez@cgiar.org>
2024-01-11 16:51:12 +0000

    Created docker file

--- src/r-capabilities/main.py
+++ src/r-capabilities/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import boto3
 import numpy as np
 import s3fs
-import utils
+from utils import utils
 from fastapi import FastAPI
 from model.models import getDataRequest
 from fastapi.responses import FileResponse
@@ -17,14 +17,19 @@ import random
 
 s3Key = utils.readConfiguration('read-access-keys','key')
 s3Secret = utils.readConfiguration('read-access-keys','secret')
+
 s3 = s3fs.S3FileSystem(key= s3Key
                        ,secret=s3Secret)
 
 app = FastAPI(docs_url="/")
 @app.post('/v1/getdataOnePoint')
-async def getData(request: getDataRequest):
+def getData(request: getDataRequest):
+    global s3
+    print("S3 Key :", request.variableName)
+    
     s3Path = utils.readConfiguration('zarr-path',request.variableName)
     rangeOfDates = pd.date_range(start=request.startDt,end=request.endDt)
+    print("S3 Key :", s3Path, "---- ")
     zarrData = utils.initializeZarrConnection(s3Path,s3)
     zarrData = utils.filterData(zarrData,
                                 rangeOfDates,
