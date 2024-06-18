d631e1875855aff6341b00ae5a7bd9bc2f06bfd4
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2024-06-18 22:08:57 +0200

    iSDAsoil dataset ready

--- src/r-scripts/resample_iSDAsoil_variables.R
+++ src/r-scripts/resample_iSDAsoil_variables.R
@@ -10,133 +10,78 @@ options(warn = -1, scipen = 999)
 suppressMessages(if(!require(pacman)){install.packages('pacman');library(pacman)} else {library(pacman)})
 suppressMessages(pacman::p_load(terra, ncdf4, readxl, gdalUtilities))
 
-src_dir <- 'D:/ToBackup/CGIAR_climate_action/datalake'
-
-## Setup table ----
-stp <- readxl::read_excel(path = paste0(src_dir,'/data/soils_metadata.xlsx'), sheet = 3) |> base::as.data.frame()
-
-u <- stp$URL[1]
-u <- paste0('/vsicurl/',u)
-
-# r <- terra::rast(u)
-
-# Temporal vrt rasters
-intrmd_vrt <- paste0(tempfile(),'.vrt') # Intermediate raster
-output_vrt <- paste0(tempfile(),'.vrt') # Final raster
-
-## GDAL operations
-# gdalbuildvrt -b 1 -resolution highest -r nearest "output.vrt" "input.tif"
-# gdalwarp -t_srs EPSG:4326 output.vrt output_warped.vrt
-
-# Resample to 5x5km
-gdalUtilities::gdalbuildvrt(gdalfile = u, output.vrt = intrmd_vrt, b = 1, resolution = 'user', tr = c(5500, 5500), r = 'bilinear')
-# Original CRS to EPSG:4326
-gdalUtilities::gdalwarp(srcfile = intrmd_vrt, dstfile = output_vrt, t_srs = 'EPSG:4326')
-
 outdir <- '//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/iSDA'
 
-# Load GDAL vrt result
-r <- terra::rast(output_vrt)
-# Write GDA: vrt result to tif
-terra::writeRaster(x = r, filename = paste0(outdir,'/',gsub('.tif','',basename(u)),'_intrmd.tif'), overwrite = T)
-r <- terra::rast(paste0(outdir,'/',gsub('.tif','',basename(u)),'_intrmd.tif'))
+## Reading setup table ----
+stp <- readxl::read_excel(path = paste0(outdir,'/soils_metadata.xlsx'), sheet = 'stp') |> base::as.data.frame()
 
-# Reference raster (CHIRPS)
-chirps_ref <- 'https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p05/2023/chirps-v2.0.2023.01.01.tif.gz'
-ref <- terra::rast(paste0('/vsigzip//vsicurl/',chirps_ref)); rm(chirps_ref)
-ref[ref == -9999] <- NA
+## Resampling function using GDAL ----
+iSDA_GDAL_processing <- function(var_url, var_name, var_band, var_depth, var_stats, var_units, long_name, outdir){
+  
+  # URL
+  u <- var_url
+  # Variable band
+  
+  # Check if the file corresponds to table or raster
+  if(strsplit(basename(u), split = '\\.')[[1]][2] == 'csv'){
+    download.file(url = u, destfile = paste0(outdir,'/',basename(u)), mode = 'wb')
+  } else {
+    u <- paste0('/vsicurl/',u)
+    
+    if(any(is.na(c(var_depth,var_stats)))){
+      outfile <- gsub('.tif','',basename(u))
+    } else {
+      outfile <- paste0(gsub('.tif','',basename(u)),'_',var_depth,'_',var_stats)
+    }
+    
+    if(!file.exists(paste0(outdir,'/',outfile,'.nc'))){
+      # Temporal vrt rasters
+      intrmd_vrt <- paste0(tempfile(),'.vrt') # Intermediate raster
+      output_vrt <- paste0(tempfile(),'.vrt') # Final raster
+      
+      ## GDAL operations
+      # gdalbuildvrt -b 1 -resolution highest -r nearest "output.vrt" "input.tif"
+      # gdalwarp -t_srs EPSG:4326 output.vrt output_warped.vrt
+      
+      # Resample to 5x5km
+      gdalUtilities::gdalbuildvrt(gdalfile = u, output.vrt = intrmd_vrt, b = var_band, resolution = 'user', tr = c(5500, 5500), r = ifelse(var_name %in% c('fcc','texture_class'),'nearest','bilinear'))
+      # Original CRS to EPSG:4326
+      gdalUtilities::gdalwarp(srcfile = intrmd_vrt, dstfile = output_vrt, t_srs = 'EPSG:4326')
+      
+      # Load GDAL vrt result
+      r <- terra::rast(output_vrt)
+      # Write GDA: vrt result to tif
+      terra::writeRaster(x = r, filename = paste0(outdir,'/',outfile,'_intrmd.tif'), overwrite = T)
+      r <- terra::rast(paste0(outdir,'/',outfile,'_intrmd.tif'))
+      names(r) <- outfile
+      
+      # Reference raster (CHIRPS)
+      chirps_ref <- 'https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p05/2023/chirps-v2.0.2023.01.01.tif.gz'
+      ref <- terra::rast(paste0('/vsigzip//vsicurl/',chirps_ref)); rm(chirps_ref)
+      ref[ref == -9999] <- NA
+      
+      # Save resampled raster into tif and nc formats
+      r_rsmp <- terra::resample(x = r, y = ref, method = ifelse(var_name %in% c('fcc','texture_class'),'near','bilinear'), threads = T, filename = paste0(outdir,'/',outfile,'.tif'), overwrite = T)
+      terra::writeCDF(x = r_rsmp, filename = paste0(outdir,'/',outfile,'.nc'), varname = var_name, longname = long_name, unit = var_units, compression = 9)
+    } else {
+      cat('File already processed\n')
+    }
+    
+  }
+  
+  return(cat('Done ... next\n'))
+  
+}
 
-# Save resampled raster into tif and nc formats
-r_rsmp <- terra::resample(x = r, y = ref, method = 'bilinear', threads = T, filename = paste0(outdir,'/',gsub('.vrt','.tif',basename(u))), overwrite = T)
-terra::writeCDF(x = r_rsmp, filename = paste0(outdir,'/',gsub('.tif','.nc',basename(u))), varname = var_name, longname = long_name, unit = var_units, compression = 9)
-
-
-
-# Read original raster
-r <- terra::rast(paste0('/vsicurl/', url))
-names(r) <- lyr_names
-
-# Reference raster (CHIRPS)
-chirps_ref <- 'https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p05/2023/chirps-v2.0.2023.01.01.tif.gz'
-ref <- terra::rast(paste0('/vsigzip//vsicurl/',chirps_ref)); rm(chirps_ref)
-ref[ref == -9999] <- NA
-
-r_proj <- terra::project(x = r, y = ref, method = 'bilinear', threads = T, filename = paste0(outfile,'.tif'), overwrite = T)
-terra::writeCDF(x = r_proj, filename = paste0(outfile,'.nc'), varname = var_name, longname = long_name, unit = var_units, compression = 9, overwrite = T)
-
-# url <- 'https://isdasoil.s3.amazonaws.com/soil_data/aluminium_extractable/aluminium_extractable.tif'
-# lyr_names <- c('aluminium_avg_00-20cm', 'aluminium_avg_20-50cm', 'aluminium_std_00-20cm', 'aluminium_std_20-50cm')
-# var_name  <- 'aluminium_extractable'
-# long_name <- 'Aluminium extractable'
-# var_units <- 'ppm'
-# outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/',gsub('.tif','',basename(url)))
-
-# url <- 'https://isdasoil.s3.amazonaws.com/soil_data/bedrock_depth/bedrock_depth.tif'
-# lyr_names <- c('bedrock_avg_00-200cm', 'bedrock_std_00-200cm')
-# var_name  <- 'bedrock_depth'
-# long_name <- 'Depth to bedrock'
-# var_units <- 'cm'
-# outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/',gsub('.tif','',basename(url)))
-
-# url <- 'https://isdasoil.s3.amazonaws.com/soil_data/bulk_density/bulk_density.tif'
-# lyr_names <- c('bulk_avg_00-20cm', 'bulk_avg_20-50cm', 'bulk_std_00-20cm', 'bulk_std_20-50cm')
-# var_name  <- 'bulk_density'
-# long_name <- 'Bulk density, <2mm fraction'
-# var_units <- 'g/cc'
-# outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/',gsub('.tif','',basename(url)))
-
-# url <- 'https://isdasoil.s3.amazonaws.com/soil_data/calcium_extractable/calcium_extractable.tif'
-# lyr_names <- c('calcium_avg_00-20cm', 'calcium_avg_20-50cm', 'calcium_std_00-20cm', 'calcium_std_20-50cm')
-# var_name  <- 'calcium_extractable'
-# long_name <- 'Calcium extractable'
-# var_units <- 'ppm'
-# outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/',gsub('.tif','',basename(url)))
-
-# url <- 'https://isdasoil.s3.amazonaws.com/soil_data/carbon_organic/carbon_organic.tif'
-# lyr_names <- c('carbon_avg_00-20cm', 'carbon_avg_20-50cm', 'carbon_std_00-20cm', 'carbon_std_20-50cm')
-# var_name  <- 'carbon_organic'
-# long_name <- 'Carbon organic'
-# var_units <- 'g/kg'
-# outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/',gsub('.tif','',basename(url)))
-
-# url <- 'https://isdasoil.s3.amazonaws.com/soil_data/carbon_total/carbon_total.tif'
-# lyr_names <- c('carbon_total_avg_00-20cm', 'carbon_total_avg_20-50cm', 'carbon_total_std_00-20cm', 'carbon_total_std_20-50cm')
-# var_name  <- 'carbon_total'
-# long_name <- 'Carbon total'
-# var_units <- 'g/kg'
-# outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/',gsub('.tif','',basename(url)))
-
-# url <- 'https://isdasoil.s3.amazonaws.com/soil_data/cation_exchange_capacity/cation_exchange_capacity.tif'
-# lyr_names <- c('cec_avg_00-20cm', 'cec_avg_20-50cm', 'cec_std_00-20cm', 'cec_std_20-50cm')
-# var_name  <- 'cation_exchange_capacity'
-# long_name <- 'Effective cation exchange capacity'
-# var_units <- 'cmol(+)/kg'
-# outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/',gsub('.tif','',basename(url)))
-
-# url <- 'https://isdasoil.s3.amazonaws.com/soil_data/clay_content/clay_content.tif'
-# lyr_names <- c('clay_avg_00-20cm', 'clay_avg_20-50cm', 'clay_std_00-20cm', 'clay_std_20-50cm')
-# var_name  <- 'clay_content'
-# long_name <- 'Clay content'
-# var_units <- '%'
-# outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/',gsub('.tif','',basename(url)))
-
-# url <- 'https://isdasoil.s3.amazonaws.com/soil_data/fcc/fcc.tif'
-# lyr_names <- c('fcc')
-# var_name  <- 'fcc'
-# long_name <- 'Fertility Capability Classification'
-# var_units <- 'FCC categories'
-# outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/',gsub('.tif','',basename(url)))
-
-# url <- 'https://isdasoil.s3.amazonaws.com/soil_data/iron_extractable/iron_extractable.tif'
-# lyr_names <- c('iron_avg_00-20cm', 'iron_avg_20-50cm', 'iron_std_00-20cm', 'iron_std_20-50cm')
-# var_name  <- 'iron_extractable'
-# long_name <- 'Iron extractable'
-# var_units <- 'ppm'
-# outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/',gsub('.tif','',basename(url)))
-
-url <- 'https://isdasoil.s3.amazonaws.com/soil_data/magnesium_extractable/magnesium_extractable.tif'
-lyr_names <- c('magnesium_avg_00-20cm', 'magnesium_avg_20-50cm', 'magnesium_std_00-20cm', 'magnesium_std_20-50cm')
-var_name  <- 'magnesium_extractable'
-long_name <- 'Magnesium extractable'
-var_units <- 'ppm'
-outfile   <- paste0('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/iSDA/',gsub('.tif','',basename(url)))
+## Loop over setup table ----
+for(i in 1:nrow(stp)){
+  iSDA_GDAL_processing(var_url = stp$url[i],
+                       var_name = stp$var_name[i],
+                       var_band = stp$var_band[i],
+                       var_depth = stp$var_depth[i],
+                       var_stats = stp$var_stats[i],
+                       var_units = stp$var_units[i],
+                       long_name = stp$long_name[i],
+                       outdir = outdir)
+  cat(paste0('Progress ',round(i/nrow(stp) * 100, 2),'%, Row: ',i,'\n'))
+}
